ea89900841baa421e10077402eac39db3aa7125b
Jhewu <junyi.hewu2003@gmail.com>
2024-12-14 15:58:58 -0500

    adapted the train test val split

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This script takes in a BraTS training directory 
+split_train_val_test is a script takes in a BraTS training directory 
 and splits it into training, validation and testing
 set. The reason being the only dataset containing 
 ground truths is in the BraTS training set. If you're, 
@@ -6,3 +6,9 @@ late to the competition, you're unable to validate or
 test your model, therefore we need to split the training
 set and use it to validate and test our model. The scripts 
 also uses threading to make this faster. 
+
+Meanwhile split_train_val_test_YOLO is a version of the 
+split_train_val_test, and it's used to prepare the dataset
+for YOLO. The train_data folder should already contain the slices,
+therefore we are creating 3 separate directories and copying the 
+respective split onto the folders. 
--- split_train_val_test_YOLO.py
+++ split_train_val_test_YOLO.py
@@ -1,17 +1,9 @@
 """
-This script takes in a BraTS training directory 
-and splits it into training, validation and testing
-set. The reason being the only dataset containing 
-ground truths is in the BraTS training set. If you're, 
-late to the competition, you're unable to validate or
-test your model, therefore we need to split the training
-set and use it to validate and test our model. The scripts 
-also uses threading to make this faster. 
-
-This scripts maintains the same directory as the source directory. 
-For example, within train_data there will be a directories of 
-different patients, and within those patients, there will be directories
-of the 4 modalities which contains the all of the 2d scans
+This script is a version of the split_train_val_test, and 
+it's used to prepare the dataset for YOLO. The train_data 
+folder should already contain the slices, therefore we are 
+creating 3 separate directories and copying the respective 
+split onto the folders. 
 """
 
 """Imports"""
@@ -32,43 +24,43 @@ def CreateDir(folder_name):
    if not os.path.exists(folder_name):
        os.makedirs(folder_name) 
        
-def CopyDir(src_list, src_base, dest_base):
-    for dir in src_list:
-        src_dir = os.path.join(src_base, dir)
-        dest_dir = os.path.join(dest_base, dir)
+def CopyFile(src_list, src_base, dest_base):
+    for file in src_list:
+        src_dir = os.path.join(src_base, file)
+        dest_dir = os.path.join(dest_base, file)
         if os.path.exists(src_dir):
-            shutil.copytree(src_dir, dest_dir)
+            shutil.copy(src_dir, dest_dir)
         else:
             print(f"Source directory does not exist: {src_dir}")
 
 """Main Runtime"""
-def Split_Train_Val_Test(): 
+def Split_Train_Val_Test_YOLO(): 
     # set up cwd and training and validation paths
     root_dir = os.getcwd()
     training_dir = os.path.join(root_dir, TRAINING_FOLDER)
 
     # list of directories in training and validation
-    patients_train_list = os.listdir(training_dir)
-    patients_length = len(patients_train_list)
-    print(f"There is a total of: {patients_length} patients in the directory\n")
+    slices_list = os.listdir(training_dir)
+    slices_length = len(slices_list)
+    print(f"There is a total of: {slices_length} slices in the directory\n")
 
     # creating the split through indexes
-    train_index = ceil(patients_length*TRAIN_SPLIT)
+    train_index = ceil(slices_length*TRAIN_SPLIT)
     print(f"Splitting... training set is {train_index} long")
 
-    val_index = ceil(patients_length*VAL_TEST_SPLIT)
+    val_index = ceil(slices_length*VAL_TEST_SPLIT)
     print(f"Splitting... validation set is {val_index} long")
 
-    test_index = patients_length-train_index-val_index
+    test_index = slices_length-train_index-val_index
     print(f"Splitting... validation set is {test_index} long")
 
     # randomly shuffle the list before splitting 
-    shuffle(patients_train_list)
+    shuffle(slices_list)
 
     # create the respective train, val and test split
-    train_list = patients_train_list[:train_index]
-    val_list = patients_train_list[train_index:train_index+val_index]
-    test_list = patients_train_list[train_index+val_index:]
+    train_list = slices_list[:train_index]
+    val_list = slices_list[train_index:train_index+val_index]
+    test_list = slices_list[train_index+val_index:]
 
     # create directories for each split
     train_dest = "dataset_split/train/"
@@ -82,17 +74,17 @@ def Split_Train_Val_Test():
     threads = []
 
     # Create and start thread for training data
-    train_thread = threading.Thread(target=CopyDir, args=(train_list, training_dir, train_dest))
+    train_thread = threading.Thread(target=CopyFile, args=(train_list, training_dir, train_dest))
     threads.append(train_thread)
     train_thread.start()
 
     # Create and start thread for validation data
-    val_thread = threading.Thread(target=CopyDir, args=(val_list, training_dir, val_dest))
+    val_thread = threading.Thread(target=CopyFile, args=(val_list, training_dir, val_dest))
     threads.append(val_thread)
     val_thread.start()
 
     # Create and start thread for test data
-    test_thread = threading.Thread(target=CopyDir, args=(test_list, training_dir, test_dest))
+    test_thread = threading.Thread(target=CopyFile, args=(test_list, training_dir, test_dest))
     threads.append(test_thread)
     test_thread.start()
 
@@ -103,5 +95,5 @@ def Split_Train_Val_Test():
     print("All directories copied successfully.")
 
 if __name__ == "__main__": 
-    Split_Train_Val_Test()
+    Split_Train_Val_Test_YOLO()
     print("\nFinish splitting the dataset, please check your directory for dataset_split folder\n")
